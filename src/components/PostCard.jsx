d345c9a8c6c45828bb98d4ddb76806c89fcb0b29
George <alximik15@gmail.com>
2023-07-14 21:13:25 +0400

    Able to create and update page

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,14 +1,33 @@
-export const PostCard = ({post}) => {
-  const {title, description, author} = post;
+import {doc, deleteDoc} from 'firebase/firestore'
+import { auth,db } from '../firebase/config';
+import { useNavigate } from 'react-router-dom';
 
-return (
-  <div className="card">
+export const PostCard = ({ post }) => {
+  const navigate = useNavigate();
+  const { id, title, description, author } = post;
+  const isAuth = JSON.parse(localStorage.getItem('isAuth'));
+
+
+async function handleDelete(){
+const document =  doc(db,'post', id)
+
+await deleteDoc(document);
+navigate('/')
+
+}
+
+  return (
+    <div className="card">
       <p className="title">{title}</p>
       <p className="description">{description}</p>
       <p className="control">
-          <span className="author">{author}</span>
-          <span className="delete"><i className="bi bi-trash3"></i></span>
+        {author && author.name && <span className="author">{author.name}</span>}
+        {isAuth && author && author.id === auth.currentUser.uid && (
+          <span onClick={handleDelete} className="delete">
+            <i className="bi bi-trash3"></i>
+          </span>
+        )}
       </p>
-  </div>
-)
-}
+    </div>
+  );
+};
