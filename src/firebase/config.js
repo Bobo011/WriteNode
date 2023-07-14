208b535ac8e52f2c85de49b347860fff4e1f1af4
George <alximik15@gmail.com>
2023-07-14 15:04:33 +0400

    Installed basic firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjWybwjyb-bWHyTpLDuZzIWkNY1XY4HP8",
+  authDomain: "boreal-analog-239910.firebaseapp.com",
+  projectId: "boreal-analog-239910",
+  storageBucket: "boreal-analog-239910.appspot.com",
+  messagingSenderId: "937524896619",
+  appId: "1:937524896619:web:47f9c45668417221d5622e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
