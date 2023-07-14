1b62feec994290c5791a3d4a503809f405e0c6ce
George <alximik15@gmail.com>
2023-07-14 14:36:32 +0400

    Pages styling done

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+export const PostCard = ({post}) => {
+  const {title, description, author} = post;
 
-export const PostCard = () => {
-  return (
-    <div>PostCard</div>
-  )
+return (
+  <div className="card">
+      <p className="title">{title}</p>
+      <p className="description">{description}</p>
+      <p className="control">
+          <span className="author">{author}</span>
+          <span className="delete"><i className="bi bi-trash3"></i></span>
+      </p>
+  </div>
+)
 }
