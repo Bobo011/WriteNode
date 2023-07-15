e366423aefa13f246cd04065ebd4f8c172229eed
George <alximik15@gmail.com>
2023-07-16 00:28:43 +0400

    Final touches

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import 'react-loading-skeleton/dist/skeleton.css'
 import App from './App';
-
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
